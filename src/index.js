1b51a53aea5840afed0e8155c5fe8be423e6a874
Sabohatfrontend <ssobirova333@gmail.com>
2023-08-05 19:26:47 +0500

    feat: add reverse function in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+
+module.exports = function reverse(n) {
+    let b = 0;
+    num = Math.abs(n);
+    while (num > 0) {
+        b = b * 10 + num % 10;
+        num = Math.floor(num / 10);
+    }
+
+    return b;
 }
